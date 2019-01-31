574a188480311344596ad7dc2e912805900066fb
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-01-31 14:10:04 +0000

    added all preliminary tests for current weather

--- lib/services/current_weather.rb
+++ lib/services/current_weather.rb
@@ -20,6 +20,50 @@ class CurrentWeather
     @current_weather_data['coord']
   end
 
+  def retrieve_weather
+    @current_weather_data['weather']
+  end
+
+  def retrieve_base
+    @current_weather_data['base']
+  end
+
+  def retrieve_main
+    @current_weather_data['main']
+  end
+
+  def retrieve_visibility
+    @current_weather_data['visibility']
+  end
+
+  def retrieve_wind
+    @current_weather_data['wind']
+  end
+
+  def retrieve_clouds
+    @current_weather_data['clouds']
+  end
+
+  def retrieve_dt
+    @current_weather_data['dt']
+  end
+
+  def retrieve_sys
+    @current_weather_data['sys']
+  end
+
+  def retrieve_id
+    @current_weather_data['id']
+  end
+
+  def retrieve_name
+    @current_weather_data['name']
+  end
+
+  def retrieve_cod
+    @current_weather_data['cod']
+  end
+  
 end
 
 test = CurrentWeather.new
--- spec/current_weather_spec.rb
+++ spec/current_weather_spec.rb
@@ -8,12 +8,56 @@ describe 'WeatherApp' do
       @WeatherApp.get_current_weather_name('London')
     end
 
-    it 'should retrieve the data as a hash' do
-      expect(@WeatherApp).to be_kind_of(Hash)
-    end
+    # it 'should retrieve the data as a hash' do
+    #   expect(@WeatherApp).to be_kind_of(Hash)
+    # end
 
     it 'should retrieve the coordinates as a hash' do
       expect(@WeatherApp.retrieve_coord).to be_kind_of(Hash)
+    end
+
+    it 'should retrieve the weather as an array' do
+      expect(@WeatherApp.retrieve_weather).to be_kind_of(Array)
+    end
+
+    it 'should retrieve the base as a string' do
+      expect(@WeatherApp.retrieve_base).to be_kind_of(String)
+    end
+
+    it 'should retrieve the main as a hash' do
+      expect(@WeatherApp.retrieve_main).to be_kind_of(Hash)
+    end
+
+    it 'should retrieve the visibility as an integer' do
+      expect(@WeatherApp.retrieve_visibility).to be_kind_of(Integer)
+    end
+
+    it 'should retrieve the wind as a hash' do
+      expect(@WeatherApp.retrieve_wind).to be_kind_of(Hash)
+    end
+
+    it 'should retrieve the clouds as a hash' do
+      expect(@WeatherApp.retrieve_clouds).to be_kind_of(Hash)
+    end
+
+    it 'should retrieve the dt as a integer' do
+      expect(@WeatherApp.retrieve_dt).to be_kind_of(Integer)
+    end
+
+    it 'should retrieve the sys as a hash' do
+      expect(@WeatherApp.retrieve_sys).to be_kind_of(Hash)
+    end
+
+    it 'should retrieve the id as an integer' do
+      expect(@WeatherApp.retrieve_id).to be_kind_of(Integer)
+    end
+
+    it 'should retrieve the name as a string' do
+      expect(@WeatherApp.retrieve_name).to be_kind_of(String)
+    end
+
+    it 'should retrieve the cod as an integer' do
+      expect(@WeatherApp.retrieve_cod).to be_kind_of(Integer)
     end
 
   end
